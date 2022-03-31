d293545be4e85e1e709cc39c1f74ba33e09877c6
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2022-03-31 00:35:48 -0500

    added home page and game over routes

--- app.py
+++ app.py
@@ -7,8 +7,18 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "zelly123"
 
 @app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/new-game')
 def new_game():
     """Generate new boggle board and store it in session"""
+    
+    if 'times_played' not in session:
+        session['times_played'] = 0
+
+    if 'high_score' not in session:
+        session['high_score'] = 0
 
     session['boggle_board'] = boggle_game.make_board()
     return redirect('/play')
@@ -16,7 +26,7 @@ def new_game():
 @app.route('/play')
 def play_game():
 
-    return render_template('index.html')
+    return render_template('game.html')
 
 @app.route('/check-word')
 def check_word():
@@ -26,3 +36,21 @@ def check_word():
     response = {"result": boggle_game.check_valid_word(board, word)}
 
     return jsonify(response)
+
+@app.route('/game-over', methods=['POST'] )
+def game_over():
+    score = request.json['score']
+    
+    if score > session['high_score']:
+        session['high_score'] = score
+
+    high_score = session['high_score']
+
+    response = {"result": high_score}
+    
+    plays = session['times_played']
+    plays += 1
+    session['times_played'] = plays
+
+    return jsonify(response)
+    
